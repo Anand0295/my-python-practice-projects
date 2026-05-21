234c8cee113cffbe19dc0f802ffdfbe5735c0e7b
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-21 16:37:01 +0000

    May 21, 2026, 10:07 PM

--- basic/rock_paper_scissors/rock_paper_scissors.py
+++ basic/rock_paper_scissors/rock_paper_scissors.py
@@ -3,4 +3,6 @@ import random
 def play():
     user = input('What's your choice? 'r' for rock, 'p' for paper, 's' for scissors. )
     computer=random.choice(['r','p','s'])
-    
+
+    if user==computer:
+        print('its a tie')
