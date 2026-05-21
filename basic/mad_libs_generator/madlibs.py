a6d04c1ddfe893a5eaf3e5ad3a2a967067e4ebec
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-21 15:29:04 +0000

    May 21, 2026, 8:59 PM

---
+++ basic/mad_libs_generator/madlibs.py
@@ -0,0 +1,19 @@
+# Mad Libs is a word game where you fill in blanks with random words to create a funny, often silly story.
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+name = input("Name: ")
+cp = input("Catchphrase: ")
+
+madlibs = f"Lightning McQueen was so {adj} that he started {verb1} on the track, then began {verb2} while shouting to {name}, “{cp}! "
+
+print(madlibs)
+"""
+Verb: drifiting
+Verb: kachow
+Name: Me
+Catchphrase: Ka-chow.
+Lightning McQueen was so awesome that he started drifiting on the track, then began kachow while shouting to Me, “Ka-chow.! 
+"""
